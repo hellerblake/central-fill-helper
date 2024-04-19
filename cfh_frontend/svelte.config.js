8a9519acb85081f3fe6cdf9ef358510efd7feb56
Blake Heller <hellerblake@gmail.com>
2024-04-18 21:59:23 -0700

    add tailwind

--- cfh_frontend/svelte.config.js
+++ cfh_frontend/svelte.config.js
@@ -1,7 +1,10 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
 		adapter: adapter()
-	}
+	},
+
+	preprocess: [vitePreprocess({})]
 };
