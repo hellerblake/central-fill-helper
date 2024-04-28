384bfe661d1951cbb1712433e1398540235b0727
Blake Heller <hellerblake@gmail.com>
2024-04-28 15:06:47 -0700

    authentication for homepage

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
+	import AuthGuard from '$lib/components/auth-guard.svelte';
 	import FillRecord from '$lib/components/fill-record.svelte';
 </script>
-
-<FillRecord />
+<AuthGuard>
+  <FillRecord />
+</AuthGuard>
